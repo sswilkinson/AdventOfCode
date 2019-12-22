611bd1fdfd7e9c2eda65a02000fbae16ba6472b7
Samuel Wilkinson <sswilkinson@users.noreply.github.com>
2019-12-22 01:43:21 +0100

    Add comment

--- 2019_cpp/source/main.cpp
+++ 2019_cpp/source/main.cpp
@@ -9,7 +9,7 @@ int main()
 	if (FuelCounterUpper::CalculateRequiredFuelForMass(14) != 2)                 return 1;
 	if (FuelCounterUpper::CalculateRequiredFuelForMass(1969) != 654)             return 1;
 	if (FuelCounterUpper::CalculateRequiredFuelForMass(100756) != 33583)         return 1;
-	if (FuelCounterUpper::CalculateRequiredFuelForMass(Day1::input) != 3332538)  return 1;
+	if (FuelCounterUpper::CalculateRequiredFuelForMass(Day1::input) != 3332538)  return 1;  // Part 1 answer
 
 	return 0;
 }
