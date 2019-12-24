24fdfbfad572eb9d4ce0def9466d26f5cb12160c
Samuel Wilkinson <sswilkinson@users.noreply.github.com>
2019-12-25 00:54:38 +0100

    Add Process() flow, Add() and Multiply() stubs

--- 2019_cpp/include/intcode_computer/IntcodeComputer.h
+++ 2019_cpp/include/intcode_computer/IntcodeComputer.h
@@ -5,5 +5,15 @@
 class IntcodeComputer
 {
 public:
+
+	enum Instructions { ADD = 1, MULTIPLY = 2, STOP = 99 };
+	static std::vector<int> FAILED_OUTPUT;
+
 	static std::vector<int> Process(const std::vector<int>& input);
+
+
+private:
+
+	static void Add(std::vector<int>& vector, unsigned int index);
+	static void Multiply(std::vector<int>& vector, unsigned int index);
 };
--- 2019_cpp/source/intcode_computer/IntcodeComputer.cpp
+++ 2019_cpp/source/intcode_computer/IntcodeComputer.cpp
@@ -1,6 +1,71 @@
 #include "intcode_computer/IntcodeComputer.h"
 
+std::vector<int> IntcodeComputer::FAILED_OUTPUT = {};
+
 std::vector<int> IntcodeComputer::Process(const std::vector<int>& input)
 {
-	return {};
+	if (input.size() == 0)
+	{
+		return FAILED_OUTPUT;
+	}
+
+	auto output = input;
+	int currentIndex = 0;
+	int numberOfElements = output.size();
+	bool reachedStopInstruction = false;
+	bool failedInstruction = false;
+
+	do
+	{
+		const int instruction = output[currentIndex];
+
+		if (instruction == Instructions::STOP)
+		{
+			reachedStopInstruction = true;
+			break;
+		}
+
+		if (currentIndex + 3 >= numberOfElements)
+		{
+			return FAILED_OUTPUT;  // Not enough input to perform the below instructions
+		}
+
+		if (instruction == Instructions::ADD)
+		{
+			failedInstruction = !Add(output, currentIndex + 1);
+		}
+		else if (instruction == Instructions::MULTIPLY)
+		{
+			failedInstruction = !Multiply(output, currentIndex + 1);
+		}
+		else
+		{
+			return FAILED_OUTPUT;  // Bad instruction
+		}
+
+		if (failedInstruction)
+		{
+			return FAILED_OUTPUT;
+		}
+
+		currentIndex += 4;
+
+	} while (currentIndex < numberOfElements);
+
+	if (!reachedStopInstruction)
+	{
+		return FAILED_OUTPUT;
+	}
+
+	return output;
+}
+
+bool IntcodeComputer::Add(std::vector<int>& vector, unsigned int index)
+{
+	return false;
+}
+
+bool IntcodeComputer::Multiply(std::vector<int>& vector, unsigned int index)
+{
+	return false;
 }
